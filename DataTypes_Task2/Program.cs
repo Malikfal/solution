ea2b7efc72a0d2fe43196310e968c79368c9bf8a
pugin_2015 <pugin_2015@mail.ru>
2026-02-27 13:11:52 +0300

    Исправление: Типы данных. Задание 2

--- DataTypes_Task2/Program.cs
+++ DataTypes_Task2/Program.cs
@@ -10,6 +10,32 @@ namespace DataTypes_Task2
     {
         static void Main(string[] args)
         {
+            string diamant = s_CreateDiamant(7);
+            s_PrintDiamant(diamant);
+
+
+        }
+
+        public static string s_CreateDiamant(int size)
+        {
+
+            var result = new StringBuilder();
+            int mid = size / 2;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (Math.Abs(i - mid) + Math.Abs(j - mid) == mid) result.Append("x");
+                    else result.Append(" ");
+                }
+                result.AppendLine();
+            }
+            return result.ToString();
+        }
+
+        public static void s_PrintDiamant(string diamant)
+        {
+            Console.Write(diamant);
         }
     }
 }
